2b36e382bbc998572be34532de2f4a2f17269cce
ANDRII VOLKOV <volkovandrij915@gmail.com>
2026-03-28 13:19:40 +0100

    Update twrp_m13.mk

--- twrp_m13.mk
+++ twrp_m13.mk
@@ -5,19 +5,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a13/device.mk)
+$(call inherit-product, device/samsung/m13/device.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a13/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/m13/recovery/root,recovery/root)
 
 # Device identifier
-PRODUCT_RELEASE_NAME := a13
-PRODUCT_DEVICE := a13
-PRODUCT_NAME := twrp_a13
+PRODUCT_RELEASE_NAME := m13
+PRODUCT_DEVICE := m13
+PRODUCT_NAME := twrp_m13
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Galaxy A13
+PRODUCT_MODEL := Galaxy M13
 PRODUCT_MANUFACTURER := samsung
 
